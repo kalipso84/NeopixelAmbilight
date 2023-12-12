65415e11b570dfcb8d0bd92f8d79270c01e5d3c7
Matic Knez <66117192+kalipso84@users.noreply.github.com>
2023-12-12 22:58:02 +0100

    Add files via upload

---
+++ code/c/spidev2/build/apps/loopback_example_app/CMakeFiles/loopback_example_app.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/c/spidev2/spidevpp/apps/loopback_example_app/main.cc" "/home/pi/c/spidev2/build/apps/loopback_example_app/CMakeFiles/loopback_example_app.dir/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "apps/loopback_example_app"
+  "/home/pi/c/spidev2/spidevpp/apps/loopback_example_app"
+  "/home/pi/c/spidev2/spidevpp/libs/spidevpp/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/c/spidev2/build/libs/spidevpp/CMakeFiles/spidevpp.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/c/spidev2/build/libs/spidevpp/CMakeFiles/spidevpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spidevpp.dir/gpio.cc.o"
+  "CMakeFiles/spidevpp.dir/spi.cc.o"
+  "libspidevpp.a"
+  "libspidevpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spidevpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
